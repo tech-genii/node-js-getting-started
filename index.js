e18e1a49b068c3b1ec8e9f789e6f21bd6ce82274
saif <saifullah.khan@99array.com>
2019-10-26 02:34:28 +0530

    Changes done to add babel

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
+import {todos} from './db/db';
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const app = express();
+
+app.get('/api/v1/todos',(req,res)=>{
+  res.status(200).send({
+    success: 'true',
+    message:'todos retrieved successfuly',
+    todos:todos
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
+
+// express()
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
